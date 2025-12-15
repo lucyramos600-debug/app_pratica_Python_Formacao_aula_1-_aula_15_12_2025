eba4cc3317718bc35f788143ce644cfb16b696c1
lucyramos600-debug <lucyramos600@gmail.com>
2025-12-15 13:06:25 -0100

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ with st.sidebar:
 if menu == "Início":
     with st.expander ("**Sobre o Instituto Nacional de Estatística**"):
         st.write("Acesse o site www.ine.cv")
-        st.image("logotipo_INE.png")
+        st.image("logotipo_INE.PNG")
 
 if menu == "Widgets":
     bt = st.button("Dê um clique")
