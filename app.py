418ee222a4fb08db694342b59f30313e084dd850
lucyramos600-debug <lucyramos600@gmail.com>
2025-12-15 11:14:32 -0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
-
-
-
-
-
-
+from streamlit_option_menu import option_menu
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import time 
+import re
+from datetime import date
 
 st.header("Introduzindo os elementos do Streamlit")
 
